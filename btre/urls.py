af1ea2255f2b76d95e7916fe9a48ba12ee3f2526
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-10-13 17:42:10 +0300

    media & admin_panel & customize_admin_panel

--- btre/urls.py
+++ btre/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path(r'^admin/', admin.site.urls),
-]
+    path('listings/', include('listings.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
