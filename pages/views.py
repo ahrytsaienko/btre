364e090107b1c62ab405832846d75c03980176e2
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-10-14 12:35:53 +0300

    index + about

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from listings.models import Listing
+from realtors.models import Realtor
 
 def index(req):
-    return render(req, 'pages/index.html')
+    # limit 3 listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+      'listings': listings
+      }
+
+    return render(req, 'pages/index.html', context)
 
 def about(req):
-    return render(req, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+      'realtors': realtors,
+      'mvp_realtors': mvp_realtors
+    }
+
+    return render(req, 'pages/about.html', context)
