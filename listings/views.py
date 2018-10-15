cdcbb06ec5b54917451984df27250debfefd5d40
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-10-15 11:46:24 +0300

    search

--- listings/views.py
+++ listings/views.py
@@ -2,30 +2,68 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
+
 
 def index(req):
-  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-  paginator = Paginator(listings, 1)
-  page = req.GET.get('page')
-  paged_listings = paginator.get_page(page)
+    paginator = Paginator(listings, 1)
+    page = req.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
-  context = {
-    'listings': paged_listings
-  }
+    context = {
+        'listings': paged_listings
+    }
 
-  return render(req, 'listings/listings.html', context)
+    return render(req, 'listings/listings.html', context)
 
 
 def listing(req, listing_id):
-  listing = get_object_or_404(Listing, pk=listing_id)
+    listing = get_object_or_404(Listing, pk=listing_id)
 
-  context = {
-    'listing': listing
-  }
+    context = {
+        'listing': listing
+    }
 
-  return render(req, 'listings/listing.html', context)
+    return render(req, 'listings/listing.html', context)
 
 
 def search(req):
-  return render(req, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in req.GET:
+        keywords = req.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    if 'city' in req.GET:
+        city = req.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    if 'state' in req.GET:
+        state = req.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    if 'bedrooms' in req.GET:
+        bedrooms = req.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    if 'price' in req.GET:
+        price = req.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': req.GET
+    }
+
+    return render(req, 'listings/search.html', context)
