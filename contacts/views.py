70cf4a32f4649c1a42c62d77aadad97ce1fdd5f2
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-10-16 19:28:11 +0300

    email + contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
 
-# Create your views here.
+from .models import Contact
+
+
+def contact(req):
+    if req.method == 'POST':
+        listing_id = req.POST['listing_id']
+        listing = req.POST['listing']
+        name = req.POST['name']
+        email = req.POST['email']
+        phone = req.POST['phone'] or ''
+        message = req.POST['message'] or ''
+        user_id = req.POST['user_id']
+        realtor_email = req.POST['realtor_email']
+
+        if req.user.is_authenticated:
+            user_id = req.user.id
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    req, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
+        contact = Contact(listing=listing,
+                          listing_id=listing_id,
+                          name=name,
+                          email=email,
+                          phone=phone,
+                          message=message,
+                          user_id=user_id)
+
+        contact.save()
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing +
+            '. Sign into the admin panel for more info',
+            'temsan89@gmail.com',
+            ['temsan89@gmail.com'],
+            fail_silently=False
+        )
+
+        messages.success(req, 'Your request has been submitted')
+        return redirect('/listings/'+listing_id)
